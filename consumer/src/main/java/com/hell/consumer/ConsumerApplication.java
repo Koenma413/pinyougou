4b3d9d98c6dc6d4d83be590b957fcbed2ceb7a72
Zeus <koenma413@sina.com>
2018-11-29 14:36:49 +0800

    解决 Whitelabel Error Page
    Whitelabel Error Page
    This application has no explicit mapping for /error, so you are seeing this as a fallback.
    
    Thu Nov 29 13:19:06 CST 2018
    There was an unexpected error (type=Not Found, status=404).
    No message available
    
    解决: 问题原因 消费服务方UserService用错注解 @Service
    UserService类是个控制器  应该使用@RestController注解

--- consumer/src/main/java/com/hell/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/hell/consumer/ConsumerApplication.java
@@ -2,8 +2,9 @@ package com.hell.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.hell.consumer")
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- consumer/src/main/java/com/hell/consumer/UserService.java
+++ consumer/src/main/java/com/hell/consumer/UserService.java
@@ -4,16 +4,19 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.hell.provider.api.IService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+
+@RestController
 public class UserService {
 
     @Reference
     IService iService;
 
-    @RequestMapping(name = "/hello")
-    public void hello() {
+    @RequestMapping(name = "/")
+    public String hello() {
         String ticket = iService.getTicket();
         System.out.println("买到票了: " + ticket);
+        return "买到票了: " + ticket;
     }
 }
